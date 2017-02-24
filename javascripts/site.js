25ee7efe4dd46c30f72e5601067ec70057a026a3
Paul M Furley <paul@paulfurley.com>
2017-02-24 12:00:17 +0000

    Automated commit at 2017-02-24 12:00:17 UTC by middleman-deploy 2.0.0-alpha

--- javascripts/site.js
+++ javascripts/site.js
@@ -1,1 +1,26 @@
-// This is where it all goes :)
+'use strict';
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function getGoalProperty(name) {
+  return JSON.parse(localStorage.getItem('goal'))[name];
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+function addClass(el, className) {
+  if (el.classList)
+     el.classList.add(className);
+  else
+     el.className += ' ' + className;
+}
