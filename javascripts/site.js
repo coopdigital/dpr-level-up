58b0c13023ec1447c1672fdbb58264f0a1ddd4e7
Ian Drysdale <ian.drysdale@co-operative.coop>
2017-02-27 11:26:13 +0000

    Automated commit at 2017-02-27 11:26:13 UTC by middleman-deploy 2.0.0-alpha

---
+++ javascripts/site.js
@@ -0,0 +1,30 @@
+'use strict';
+
+function ready(fn) {
+  if (document.readyState != 'loading'){
+    fn();
+  } else {
+    document.addEventListener('DOMContentLoaded', fn);
+  }
+}
+
+function getGoalProperty(name) {
+  return JSON.parse(localStorage.getItem('goal'))[name];
+}
+
+function getEnhancementProperty(name) {
+  return JSON.parse(localStorage.getItem('enhancement'))[name];
+}
+
+function removeClass(el, className) {
+  if (el.classList)
+    el.classList.remove(className);
+  else
+    el.className = el.className.replace(new RegExp('(^|\\b)' + className.split(' ').join('|') + '(\\b|$)', 'gi'), ' ');
+}
+function addClass(el, className) {
+  if (el.classList)
+     el.classList.add(className);
+  else
+     el.className += ' ' + className;
+}
